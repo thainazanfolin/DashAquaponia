d9f6fb942ff5b8cc8b04678dd79c3c3a0eceae4f
teuzfavetta <matfavetta@gmail.com>
2023-05-16 20:09:28 -0300

    added logo on browser tab and footer changes

--- DashAquaponia/static/js/card.js
+++ DashAquaponia/static/js/card.js
@@ -16,33 +16,33 @@ botao1.addEventListener('click', function() {
 });
 
 
-const botao2 = document.getElementById('botao_ler2');
+// const botao2 = document.getElementById('botao_ler2');
 
-botao2.addEventListener('click', function() {
-    var card2 = document.querySelector('.card2');
+// botao2.addEventListener('click', function() {
+//     var card2 = document.querySelector('.card2');
     
-    card2.classList.toggle('active');
+//     card2.classList.toggle('active');
 
-    if (card2.classList.contains('active')) {
-        return botao2.textContent = 'Ler menos';
-    }
+//     if (card2.classList.contains('active')) {
+//         return botao2.textContent = 'Ler menos';
+//     }
     
-    botao2.textContent = 'Ler mais';
-});
+//     botao2.textContent = 'Ler mais';
+// });
 
 
 
-const botao3 = document.getElementById('botao_ler3');
+// const botao3 = document.getElementById('botao_ler3');
 
-botao3.addEventListener('click', function() {
-    var card3 = document.querySelector('.card3');
+// botao3.addEventListener('click', function() {
+//     var card3 = document.querySelector('.card3');
     
-    card3.classList.toggle('active');
+//     card3.classList.toggle('active');
 
-    if (card3.classList.contains('active')) {
-        return botao3.textContent = 'Ler menos';
-    }
+//     if (card3.classList.contains('active')) {
+//         return botao3.textContent = 'Ler menos';
+//     }
     
-    botao3.textContent = 'Ler mais';
-});
+//     botao3.textContent = 'Ler mais';
+// });
 
--- PI/settings.py
+++ PI/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DashAquaponia.apps.DashaquaponiaConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
